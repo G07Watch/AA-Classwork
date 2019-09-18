ed633b9e2f2ce44f5b91a6ec96d390ca313fc376
Garvin <gming27@gmail.com>
2019-09-18 19:39:17 -0400

    Adds AjaxTwitter and CSS Friends to W12D3

---
+++ W12D3/skeleton/frontend/twitter.js
@@ -0,0 +1,8 @@
+import FollowToggle from './follow_toggle.js';
+import UserSearch from './users_search.js';
+
+$(() => {
+  let ft = new FollowToggle ($(".follow-toggle"));
+  let search = new UserSearch ($(".user-search"));
+
+});
