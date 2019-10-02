08d9de36bd62c4ce7fbac5f2809c2e8c4542a6c5
Garvin <gming27@gmail.com>
2019-10-02 19:54:11 -0400

    Adds FrontEndAuth and BenchBnB to W14D3

---
+++ W14D3/benchbnb/app/controllers/api/users_controller.rb
@@ -0,0 +1,29 @@
+class Api::UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    if @user
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 404
+    end
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      log_in(@user)
+      redirect_to api_user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new # fix later
+    end
+  end
+
+  
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
